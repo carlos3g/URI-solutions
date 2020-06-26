03ea59b11712915f0b6a6649197fec4713b80339
carlos3g <carlosmesquita156@gmail.com>
2020-06-26 15:52:53 -0300

    adding initial python problems

---
+++ python/1001.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+A = int(input())
+B = int(input())
+
+x = A+B
+
+print("X =", x)
---
+++ python/1002.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+raio = float(input())
+
+area = 3.14159*raio**2
+
+print("A=%.4f" % area)
